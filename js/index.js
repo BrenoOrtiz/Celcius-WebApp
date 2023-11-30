4e19d311704a4fca0b07ac769404366bddefb7c5
Breno <breno.ortizdavid@outlook.com>
2023-11-30 18:16:21 -0300

    slider implementation
    slider of weather in hour by hour cards

--- js/index.js
+++ js/index.js
@@ -2,15 +2,16 @@
 const APIKEY = "0d521a846cb3440a94710259230211";
 const APIURL = "https://api.weatherapi.com/v1/forecast.json";
 const APIURL_SEARCH = "https://api.weatherapi.com/v1/search.json";
+
 var containerCurrent = document.getElementById('day-info-container');
+var containerCurrentHours = document.getElementById('day-hours-container');
 var containerForecast = document.getElementById('forecast-data');
-
-var body = document.querySelector('body');
-var root = document.createElement(':root');
-
-var isDay = false;
  
-var forecastHeading = document.querySelector('.forecast-heading');
+var forecastContainer = document.querySelector('.forecast-container');
+
+var sliderParentContainer = document.getElementById('slider-parent-container');
+var sliderContainer = document.getElementById('slider-container');
+
 
 
 async function loadData(input) {
@@ -24,64 +25,72 @@ async function loadData(input) {
         var dataLocation = response.location;
         // Dados do tempo atuais
         var dataCurrent = response.current;
+        console.log(dataCurrent);
         // Dados da previsão do tempo dos prox 10 dias
         var dataforecast = response.forecast.forecastday;
+        console.log(dataforecast);
 
-        forecastHeading.style.display = 'flex';
+        forecastContainer.style.display = 'flex';
+        sliderParentContainer.style.display = 'flex';
         
         if (dataCurrent.is_day) { // Caso o local pesquisado esteja dia
-            isDay = true;
-            body.style.backgroundImage = "url('../img/sun-background.png')";
             containerCurrent.innerHTML = `
             <div class="day-info-top">
-                <span class="primary-text">Hoje</span>
-                <p id="date" class="secondary-text">${dataLocation.localtime}</p>
-                </div>
-                <div class="day-info-bottom">
-                <h2 id="temperature" class="primary-text">${dataCurrent.temp_c}°C</h2>
-                <p id="place" class="secondary-text">${dataLocation.name}, ${dataLocation.country} <i class="fa-solid fa-sun" style="color: #ffd500;"></i></p>
-            </div>`;
-        } else {  // Caso o local pesquisado esteja noite
-            isday = false;
-            body.style.backgroundImage = "url('../img/moon-background.png')";
-            containerCurrent.innerHTML = `
-            <div class="day-info-top">
-            <span class="primary-text">Hoje</span>
-            <p id="date" class="secondary-text">${dataLocation.localtime}</p>
+                <span>Hoje</span>
+                <p id="date">${dataLocation.localtime.replace(/-/g, '/')}</p>
             </div>
             <div class="day-info-bottom">
-            <h2 id="temperature" class="primary-text">${dataCurrent.temp_c}°C</h2>
-            <p id="place" class="secondary-text">${dataLocation.name}, ${dataLocation.country} <i class="fa-solid fa-moon" style="color: #422475;"></i></p>
+                <h2 id="temperature">${dataCurrent.temp_c}°C</h2>
+                <p id="place">${dataLocation.name}, ${dataLocation.country} <i class="fa-solid fa-sun" style="color: #ffd500;"></i></p>
+            <div class="tempo-atual-container">
+                <span class="desc-tempo">${dataCurrent.condition.text}</span>
+                <img src="${dataCurrent.condition.icon}" alt="tempo-atual-img">
+            </div>
+            </div>`;
+        } else {  // Caso o local pesquisado esteja noite
+            containerCurrent.innerHTML = `
+            <div class="day-info-top">
+                <span>Hoje</span>
+                <p id="date">${dataLocation.localtime.replace(/-/g, '/')}</p>
+            </div>
+            <div class="day-info-bottom">
+                <h2 id="temperature">${dataCurrent.temp_c}°C</h2>
+                <p id="place">${dataLocation.name}, ${dataLocation.country} <i class="fa-solid fa-moon" style="color: #422475;"></i></p>
+            <div class="tempo-atual-container">
+                <span class="desc-tempo">${dataCurrent.condition.text}</span>
+                <img src="${dataCurrent.condition.icon}" alt="tempo-atual-img">
+            </div>
             </div>`;
         }
+
+        containerCurrentHours.innerHTML = ``;
+        dataforecast[0].hour.forEach(hourInfo => {
+            containerCurrentHours.innerHTML += `
+                    <div class="hour-card">
+                        <h3 class="horario">${hourInfo.time.slice(-5)}</h3>
+                        <img src="${hourInfo.condition.icon}" alt="tempo-img">
+                        <span class="card-temp">${parseInt(hourInfo.temp_c)}°C</span>
+                    </div>`;
+        })
         
         containerForecast.innerHTML = ``; // Limpar conteúdo anterior
         for (var i = 1; i < dataforecast.length; i++) {
 
             containerForecast.innerHTML +=` 
             <div class="day-forecast-container">
-                <span id="day-forecast" class="primary-text">${dataforecast[i].date.slice(-2)}</span>
+                <span id="day-forecast">${dataforecast[i].date.slice(-2)}</span>
                 <div class="chuva-forecast-container">
                     <i class="fa-solid fa-droplet"></i>
-                    <span id="chuva-forecast" class="primary-text">${dataforecast[i].day.daily_chance_of_rain}%</span>
+                    <span id="chuva-forecast">${dataforecast[i].day.daily_chance_of_rain}%</span>
                 </div>
                 <img src="${dataforecast[i].day.condition.icon}" alt="tempo-img" height="50px" width="50px">
-                <span id="temp-min-forecast" class="primary-text">${parseInt(dataforecast[i].day.mintemp_c)}°C</span>
-                <span id="temp-avg-forecast" class="primary-text">${parseInt(dataforecast[i].day.avgtemp_c)}°C</span>
-                <span id="temp-max-forecast" class="primary-text">${parseInt(dataforecast[i].day.maxtemp_c)}°C</span>
+                <span id="temp-min-forecast">${parseInt(dataforecast[i].day.mintemp_c)}°C</span>
+                <span id="temp-avg-forecast">${parseInt(dataforecast[i].day.avgtemp_c)}°C</span>
+                <span id="temp-max-forecast">${parseInt(dataforecast[i].day.maxtemp_c)}°C</span>
             </div>
             `;
         }
 
-        if (!isDay) {
-            document.querySelectorAll('.primary-text').forEach(element => {
-                element.style.color = '#ffffff';
-            });
-            document.querySelectorAll('.secondary-text').forEach(element => {
-                element.style.color = '#ffffff';
-            });
-        }
-        
     } else {
         forecastHeading.style.display = 'none';
         if (city != "") {
@@ -128,7 +137,27 @@ async function applySuggestion(suggestion) {
     var input = document.getElementById('search-input');
     var suggestion_list = document.getElementById('suggestion-list');
     input.value = suggestion.textContent;
-
-    await loadData(input);
     suggestion_list.style.display = "none";
+
+    loadData(input);
+}
+
+
+var currentPos = 0;
+var slideWidth = 164;
+
+function slideLeft() {
+    if (currentPos > 0) {
+        currentPos -= slideWidth; 
+    }
+    containerCurrentHours.style.right = currentPos + "px"; 
+    
+}
+
+function slideRight() {
+    var qntdVisibleCard = Math.round(sliderContainer.clientWidth / 160);
+    if (currentPos < (24 - qntdVisibleCard) * 164) {
+        currentPos += slideWidth; 
+    }
+    containerCurrentHours.style.right = currentPos + "px"; 
 }
